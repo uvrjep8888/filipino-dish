8af32de4ad00da6aa2f9344651c3c3692118b25b
uvrjep8888 <uverojeffreydev@gmail.com>
2025-07-29 16:14:24 +0800

    add more logs

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -35,10 +35,17 @@ exports.getFoodByName = async (req,res) => {
 	const name = req.query.name;
 	const foods = await fetchEntry(); 	
 	const test = "adobo";
-	console.log("tester", String(test).trim().toLowerCase().includes(String(name).trim().toLowerCase()))
+	// console.log("tester", String(test).trim().toLowerCase().includes(String(name).trim().toLowerCase()))
 
-	console.log("name type", typeof name)
-	console.log("test type", typeof test)
+	// console.log("name type", typeof name)
+	// console.log("test type", typeof test)
+	
+	const safeTest = String(test).trim().toLowerCase();
+	const safeName = String(name).trim().toLowerCase();
+
+	console.log("Raw Values =>", { test, name });
+	console.log("Processed =>", { safeTest, safeName });
+	console.log("Result =>", safeTest.includes(safeName));
 
 
 	const result = foods[0].filter((food) => {
