921f255db85f149643059fa2032de71564cb8124
uvrjep8888 <uverojeffreydev@gmail.com>
2025-07-29 15:45:34 +0800

    bugfixing

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -37,7 +37,7 @@ exports.getFoodByName = async (req,res) => {
 	console.log("name-value", name)
 	console.log("getting the variable value ..... ")
 	const result = foods[0].filter((food) => {
-		console.log('food', food)
+		console.log('food', food.name.toLowerCase().includes(name.toLowerCase()))
 		return food.name.toLowerCase().includes(name.toLowerCase())
 	})
 
