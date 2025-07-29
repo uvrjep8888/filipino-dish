457275d023399ebe888adbc87fe035099fed4116
uvrjep8888 <uverojeffreydev@gmail.com>
2025-07-29 16:18:19 +0800

    Remove console

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -30,30 +30,12 @@ exports.getFoodByType = async (req, res) => {
 }
 
 exports.getFoodByName = async (req,res) => {
-
-	console.log("starting to getfoodbyname function ...... ")
 	const name = req.query.name;
 	const foods = await fetchEntry(); 	
-	const test = "adobo";
-	// console.log("tester", String(test).trim().toLowerCase().includes(String(name).trim().toLowerCase()))
-
-	// console.log("name type", typeof name)
-	// console.log("test type", typeof test)
-	
-	const safeTest = String(test).trim().toLowerCase();
-	const safeName = String(name).trim().toLowerCase();
-
-	console.log("Raw Values =>", { test, name });
-	console.log("Processed =>", { safeTest, safeName });
-	console.log("Result =>", safeTest.includes(safeName));
-
 
 	const result = foods[0].filter((food) => {
 		return food.name.toLowerCase().includes(name.toLowerCase())
 	})
 
-
-
 	res.send(result)
-
 }
