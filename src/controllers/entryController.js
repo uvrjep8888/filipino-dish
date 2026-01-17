b37c363ec8e53937028f4c6d231c5087c4e5d154
Jeffrey Uvero <jeuvero@lbcexpress.com>
2026-01-18 06:38:21 +0800

    Implement getfood by type

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -1,41 +1,54 @@
-const {fetchEntry} = require("../services/entryServices");
+const { fetchEntry } = require("../services/entryServices");
 
 exports.getEntries = async (req, res) => {
-	const entries = await fetchEntry(); 
+  const entries = await fetchEntry();
 
-	res.json(entries)
-}
-
+  res.json(entries);
+};
 
 exports.getFood = async (req, res) => {
-	const foods = await fetchEntry(); 
+  const foods = await fetchEntry();
 
-	const randomIndx = Math.floor(Math.random() * foods[0].length)
+  const randomIndx = Math.floor(Math.random() * foods[0].length);
 
-	const food = foods[0][randomIndx]
+  const food = foods[0][randomIndx];
 
-	res.json(food)
-}
+  res.json(food);
+};
 
+exports.getFoodsByType = async (req, res) => {
+  const { type } = req.params;
+  const foods = await fetchEntry();
+
+  const filteredFood = foods[0].filter((food) => {
+    return food.type == type;
+  });
+
+  res.send(filteredFood);
+};
+
+exports.getFoodByName = async (req, res) => {
+  const name = req.query.name;
+  const foods = await fetchEntry();
+
+  const result = foods[0].filter((food) => {
+    return food.name.toLowerCase().includes(name.toLowerCase());
+  });
+
+  res.send(result);
+};
 
 exports.getFoodByType = async (req, res) => {
-	const {type} = req.params
-	const foods = await fetchEntry(); 
+  const { type } = req.params;
+  const foods = await fetchEntry();
 
-	const filteredFood = foods[0].filter((food) => {
-		return food.type == type 
-	})
-	
-	res.send(filteredFood)
-}
+  const filteredFood = foods[0].filter((food) => {
+    return food.type == type;
+  });
 
-exports.getFoodByName = async (req,res) => {
-	const name = req.query.name;
-	const foods = await fetchEntry(); 	
+  const randomIndx = Math.floor(Math.random() * filteredFood.length);
 
-	const result = foods[0].filter((food) => {
-		return food.name.toLowerCase().includes(name.toLowerCase())
-	})
+  const food = filteredFood[randomIndx];
 
-	res.send(result)
-}
+  res.json(food);
+};
