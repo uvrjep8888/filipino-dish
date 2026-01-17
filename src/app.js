3d1a6c8e8ecc293ccc089efe28a9c9b477777e64
Jeffrey Uvero <jeuvero@lbcexpress.com>
2026-01-18 06:38:36 +0800

    Fix pretier

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
+// const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Welcome to the Filipino Dish app");
+});
 
-app.get('/', (req, res) => {
-	res.send('Welcome to the Filipino Dish app')
-})
+app.use(express.json());
 
-app.use(express.json())
+const entryRoutes = require("./routes/entryRoutes");
 
-const entryRoutes = require("./routes/entryRoutes")
+app.use("/api/entries", entryRoutes);
 
-app.use("/api/entries", entryRoutes)
-
-
-app.listen(PORT, ()=> {
-	console.log(`Server is running on port ${PORT}`)
-})
+app.listen(5000, () => {
+  console.log(`Server is running on port ${5000}`);
+});
 
 module.exports = app;
