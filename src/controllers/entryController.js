46ab05063338aeaecb549c4a2397ea519d90d2d0
Jeffrey Uvero <uverojeffreydev@gmail.com>
2026-02-05 14:27:40 +0800

    Create api function that return weekly meal

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -52,3 +52,59 @@ exports.getFoodByType = async (req, res) => {
 
   res.json(food);
 };
+
+exports.buildWeeklyMealPlan = async (req, res) => {
+  const foods = await fetchEntry();
+
+  const mealType = {
+    chicken: 0,
+    pork: 0,
+    seafood: 0,
+    vegetable: 0,
+  };
+
+  const maxChicken = 2;
+  const maxPork = 2;
+  const maxSeafood = 2;
+  const maxVegetable = 0;
+
+  let mealPlan = [];
+
+  let collection = [];
+  let newMeal = [];
+
+  Array.from({ length: foods[0].length }).forEach((element) => {
+    const filtered = foods[0].filter((food) => food.type);
+    const randomItem = filtered[Math.floor(Math.random() * foods[0].length)];
+    if (!foods[0].length) return null;
+
+    if (!collection.includes(randomItem.id)) {
+      collection.push(randomItem.id);
+      newMeal.push(randomItem);
+    }
+  });
+
+  newMeal.filter((food, index) => {
+    if (food.type == "chicken" && mealType.chicken != maxChicken) {
+      mealPlan.push(food);
+      mealType.chicken += 1;
+    }
+
+    if (food.type == "pork" && mealType.pork != maxPork) {
+      mealPlan.push(food);
+      mealType.pork += 1;
+    }
+
+    if (food.type == "seafood" && mealType.seafood != maxSeafood) {
+      mealPlan.push(food);
+      mealType.seafood += 1;
+    }
+
+    if (food.type == "vegetable" && mealType.vegetable != maxVegetable) {
+      mealPlan.push(food);
+      mealType.vegetable += 1;
+    }
+  });
+
+  res.json(mealPlan);
+};
